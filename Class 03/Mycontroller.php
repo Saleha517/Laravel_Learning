faca23b53d72831fbc3a885b46bc9002a10684a6
Saleha_Usman <smyself7986@gmail.com>
2025-05-30 23:05:06 -0700

    Get Method by using Controller and Route

---
+++ Class 03/Mycontroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Mycontroller extends Controller
+{
+ public function preview(){
+    return view('class 03.contactus');
+ }
+}
